5d2ee4b0ab61eb0ca0ca4017a85bdf1026198fe9
anangnov <anangnov99@gmail.com>
2020-08-16 00:45:30 +0700

    test husky

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  printWidth: 150,
+  tabWidth: 2,
+  trailingComma: 'all',
+  // Override any other rules you want
+};
