f53ce33a2745704c0edfb3fbe33bf4747cf1e1fe
anangnov <anangnov99@gmail.com>
2020-08-16 00:57:31 +0700

    test husky

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  bracketSpacing: true,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  printWidth: 150,
-  tabWidth: 2,
-  trailingComma: 'all',
-  // Override any other rules you want
-};
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 const Home = () => {
-    return(
-        <div className="mt-4 container">
-            <div className="alert alert-primary" role="alert">
-                This Home
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="mt-4 container">
+      <div className="alert alert-primary" role="alert">
+        This Home
+      </div>
+    </div>
+  );
+};
 
 export default Home;
